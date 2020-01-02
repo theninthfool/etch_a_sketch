224680476597876ba3f93382283148820ee39f28
Zach Dummerth <zdummerth@gmail.com>
2020-01-02 16:04:31 -0600

    Create js size variable to determine size of grid and use that variable in css

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let canvas = document.querySelector("#canvas");
 
+// determines size of the grid and creates the css variable --size
+let size = 32;
+document.documentElement.style.setProperty('--size', size);
 
-let size = 16;
+
+// Creates Grid
 for (let i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
     cell.className = "cell";
