d2e75b2de9fe5552dbcca4372fefef9d1639bc90
Zach Dummerth <zdummerth@gmail.com>
2020-01-02 15:53:48 -0600

    Resize grid

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.querySelector("#canvas");
 
 
-let size = 50;
+let size = 16;
 for (let i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
     cell.className = "cell";
