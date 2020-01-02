97b3109398aef32c192a4eb1ddb3c8174a9d9ee1
Zach Dummerth <zdummerth@gmail.com>
2020-01-02 17:40:14 -0600

    Create button to determine size of the grid

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let canvas = document.querySelector("#canvas");
+//creates initial grid
+let size = 16;
+createGrid(size);
 
-// determines size of the grid and creates the css variable --size
-let size = 32;
-document.documentElement.style.setProperty('--size', size);
 
+//the button deletes the old grid and creates new one
+let input = document.querySelector('#sizeInput')
+let button = document.querySelector("#sizeButton")
+button.addEventListener('click', function() {
+    size = Number(input.value);
+    deleteGrid();
+    createGrid(size);
+});
 
-// Creates Grid
-for (let i = 0; i < size * size; i++) {
-    let cell = document.createElement("div");
-    cell.className = "cell";
-    canvas.appendChild(cell);
+function createGrid(size) {
+    console.log(size);
+    document.documentElement.style.setProperty('--size', size);
+    for (let i = 0; i < size * size; i++) {
+        let cell = document.createElement("div");
+        cell.className = "cell";
+        canvas.appendChild(cell);
+    }
+}
+
+function deleteGrid() {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
 }
