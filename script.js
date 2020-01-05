96e48449a4b7a776452ef0166c2c5f56a2250d57
Zach Dummerth <zdummerth@gmail.com>
2020-01-04 18:14:00 -0600

    Add current color display, clean code

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 let color = '';
 let canvas = document.querySelector("#canvas");
 let currentColor = document.querySelector("#currentColor");
-let size = 16;
-createGrid(size);
+// let size = 16;
+createGrid(20, canvas);
+currentColor.style.background = "Black";
+chooseColor("Black");
 // --Initial Setup
 
 
@@ -17,16 +19,23 @@ resetButton.addEventListener('click', reset);
 let input = document.querySelector('#sizeInput')
 let button = document.querySelector("#sizeButton")
 button.addEventListener('click', function() {
-    size = Number(input.value);
-    deleteGrid();
-    createGrid(size);
+    let size = Number(input.value);
+    deleteGrid(canvas);
+    createGrid(size, canvas);
+    chooseColor("Black");
 });
 
 let colorButtons = Array.from(document.querySelectorAll(".colorButtons"));
 for (let i = 0; i < colorButtons.length; i++) {
     colorButtons[i].addEventListener('click', function() {
-        color = this.textContent
-        chooseColor(color);
+        let randomColor = pickRandomColor(1);
+
+        let red = randomRGBValue();
+        let blue = randomRGBValue();
+        let green = randomRGBValue();
+
+        color = this.textContent;
+        chooseColor(color, randomColor, red, green, blue);
     });
 }
 // --Buttons
@@ -34,73 +43,68 @@ for (let i = 0; i < colorButtons.length; i++) {
 
 
 // Functions
-function createGrid(size) {
+function createGrid(size, location) {
     document.documentElement.style.setProperty('--size', size);
     for (let i = 0; i < size * size; i++) {
         let newCell = document.createElement("div");
         newCell.classList.add('cell');
-        canvas.appendChild(newCell);
+        location.appendChild(newCell);
     }
-    chooseColor('Black');
 }
 
-function deleteGrid() {
-    while (canvas.firstChild) {
-        canvas.removeChild(canvas.firstChild);
+function deleteGrid(location) {
+    while (location.firstChild) {
+        location.removeChild(location.firstChild);
     }
 }
 
 function reset() {
-    deleteGrid();
-    createGrid(16);
+    deleteGrid(canvas);
+    deleteGrid(currentColor);
+    createGrid(16, canvas);
+    chooseColor("Black");
 }
 
 
-function chooseColor(color) {
+function chooseColor(color, randomColor, red, green, blue) {
+    currentColor.style.background = selectColor(color, randomColor, 1, red, green, blue);
     let cells = Array.from(document.querySelectorAll(".cell"));
-    let randomColor = randomShade(1);
-    let red = randomRGBValue();
-    let blue = randomRGBValue();
-    let green = randomRGBValue();
-
     for (let i = 0; i < cells.length; i++) {
         let shade = 0;
-        let cellColor = "";
         cells[i].addEventListener('mouseover', function() {
-            if (color === "Eraser") {
-                cellColor = "none"
-            } else if (color === "Random") {
-                cellColor = randomColor;
-            } else if (color === "Rainbow") {
-                cellColor = randomShade(1);
-            } else if (color === "Shade") {
+            if (color.indexOf("Shade") > -1) {
                 if (shade <= 1) {
                     shade += .1;
-                    cellColor = "rgba(0,0,0," + shade + ")";
                 }
-            } else if (color === "Random Shade") {
-                if (shade <= 1) {
-                    shade += .1;
-                    cellColor = "rgba(" + red + "," + blue + "," + green + "," + shade + ")";
-                }
-            } else if (color === "Rainbow Shade") {
-                if (shade <= 1) {
-                    shade += .1;
-                    cellColor = randomShade(shade);
-                }
-            } else {
-                cellColor = color;
             }
-            this.style.background = cellColor;
+            this.style.background = selectColor(color, randomColor, shade, red, green, blue);
         });
     }
 }
 
-function randomShade(alpha) {
+function pickRandomColor(alpha) {
     return "rgba(" + randomRGBValue() + "," + randomRGBValue() + "," + randomRGBValue() + "," + alpha + ")";
 }
 
 function randomRGBValue() {
     return Math.floor(Math.random() * 255)
+}
+
+function selectColor(color, randomColor, shade, red, green, blue) {
+    if (color === "Eraser") {
+        return "none";
+    } else if (color === "Random") {
+        return randomColor;
+    } else if (color === "Rainbow") {
+        return pickRandomColor(1);
+    } else if (color === "Shade") {
+        return "rgba(0,0,0," + shade + ")";
+    } else if (color === "Random Shade") {
+        return "rgba(" + red + "," + blue + "," + green + "," + shade + ")";
+    } else if (color === "Rainbow Shade") {
+        return pickRandomColor(shade);
+    } else {
+        return color;
+    }
 }
 // --Functions
